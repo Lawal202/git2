ef500a928a63de56d1609493a75de1965112a38a
Lawal202 <lawal202@gmail.com>
2024-03-20 13:34:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <p>Welcome to new react</p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Header(){
+return(
+  <h1>
+    Good Afternoon
+  </h1>
+)
+}
+
+
+function maincontent() {
+  return(
+    <div>
+      <Header/>
+      <footer/>
     </div>
   );
 }
 
-export default App;
+// export default App;
